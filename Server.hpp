77981b96ce7530654f3ca5145e466915c7da32ee
glambrig <glambrig@student.42perpignan.fr>
2024-06-10 17:06:47 +0200

    Small fix, Server.hpp

--- Server.hpp
+++ Server.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "Channel.hpp"
+#include "User.hpp"
 
 class Server
 {
 private:
 	Channel	*_channels;	//Will be _channels[nb_of_channels]
+	User	*_users;
 public:
 	Server();
 	Server(const Server& copy);
